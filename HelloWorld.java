c617ad41f09361fb1c3218947e832157f6f3ec53
nobuocom <84282606+nobuocom@users.noreply.github.com>
2021-05-16 19:46:17 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		// Prints "Hellow,Rorld" in the terminal window.
+		// Prints "Hellow,World" in the terminal window.
 		System.out.println("Hello, World2222!");
 	}
 }
