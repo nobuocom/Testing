28a0cf3093b10eba19f467a4e21c9de5ada2f42f
nobuocom <nobuocom@gmail.com>
2021-05-16 16:44:44 +0000

    bla bla bla test2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		// Prints "Hellow,Rorld" in the terminal window.
-		System.out.println("Hello, World!");
+		System.out.println("Hello, World2222!");
 	}
 }
